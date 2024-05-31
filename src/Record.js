2d887cdd975129846d522fd0d938954a529ea7c8
Acid49 <67170582+Acid49@users.noreply.github.com>
2024-06-01 02:59:53 +0900

    Update Record.js

--- src/Record.js
+++ src/Record.js
@@ -1,19 +1,38 @@
-import { View, StyleSheet} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import InputDate from "./InputDate";
 import InputPlace from './InputPlace';
 import InputFish from './InputFish';
 
+import { useState } from 'react';
+
+
 export default function Record(){
 
+   const [fishes, setFishes] = useState([]);
+   const [selectedDate, setSelectedDate] = useState("0000-00-00");
+
+    const  addFish = () => {
+        setFishes([...fishes, { fishName: '', fishLength: '', key: Date.now() }]);
+    }
+  
+    const removeFish = (index) => {
+        const updatedFishes = [...fishes];
+        updatedFishes.splice(index, 1);
+        setFishes(updatedFishes);
+    }
+
+
     return(<>
+        <View style={{height: 50}}/>
         <InputDate/>
-        <InputPlace/>
-        <InputFish/>
+        <InputPlace selectedDate={selectedDate} setSelectedDate={selectedDate}/>
+        <InputFish fishes={fishes} setFishes={setFishes} addFish={addFish} removeFish={removeFish}/>
         <StatusBar style='auto'/>
     </>)
 }
+
 
 const styles = StyleSheet.create({
     container: {
